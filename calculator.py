a6f3689cf86fe3266032f640376579cbe9ff7ad1
D-BOY <65898012+DBOYttt@users.noreply.github.com>
2022-04-23 21:12:38 +0200

    first task

---
+++ calculator.py
@@ -0,0 +1,12 @@
+print("HI im calculator maked by D-BOY")
+
+print('''
+Wybierz co chcesz zrobić 
+1 - Dodać 
+2 - Odjąć 
+3 - Podzielić
+4 - Pomnożyć
+5 - Spotengować
+''')
+
+a = int(input("wybierz: "))
